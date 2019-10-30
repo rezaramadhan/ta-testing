5ad7b1e8cc59340dbe06ad960f12b80e9a410728
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2019-10-30 10:00:31 +0700

    Get tests args from dict constant

--- run_test.py
+++ run_test.py
@@ -2,76 +2,126 @@
 import sys
 import subprocess
 import os
+import itertools
+import csv
+from copy import deepcopy
 from statistics import mean
+from pprint import pprint
 
-RUN_COUNT = 1
+RUN_COUNT = 3
 TC_COUNT = 3
-ARGS_DH = {
-    'key_size': [64*i for i in range(1,9)]
+ARGS = {
+    'dh': {
+        '1.key_size': [64 * i for i in range(1, 19)]
+    },
+    'bn': {
+        '1.tc_num': [i for i in range(1, TC_COUNT + 1)],
+        '2.opr': ['add', 'div', 'mul', 'modexp', 'add,div,mul,modexp']
+    },
+    'rsa': {
+        '1.key_size': [64 * i for i in range(1, 5)],
+        '2.e': [3, 17, 65537],
+        # '3.msg_size': [10, 100, 1000],
+    }
 }
-ARGS_BN = {
-    'tc_num': [i for i in range(3)],
-    'opr':['add', 'div', 'mul', 'modexp']
-}
-ARGS_RSA = {
-    'key_size' : [256, 512, 1024, 2048, 4096],
-    'e' : [3, 17, 65537]
-}
-RESULT_DIR = 'result_datax'
+
+RESULT_DIR = 'result_data/'
 
 def run_individual_test(args):
     command = ['./test.run']
     command.extend(args)
     result = []
-    print('Running test:', ' '.join(args), ' RUN_COUNT ', RUN_COUNT)
+    print('Running test:', ' '.join(command))
+    print('  RUN_COUNT ', str(RUN_COUNT))
     for i in range(RUN_COUNT):
         out = subprocess.run(command, stdout=subprocess.PIPE)
+        # print(out.stdout)
         result.append(float(out.stdout.decode('utf-8').rstrip()))
-    print(result)
     return result
 
-def print_file(filename, header, file_content):
+def print_file_csv(filename, header, file_content):
     with open(filename, mode='w') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=header)
         writer.writeheader()
 
         for item in file_content:
+            print(item)
             writer.writerow(item)
 
-def run_bntest():
-    pass
+def get_params(mode):
+    command_args = []
+    # var_lists =
+    for var1 in sorted(ARGS[mode].keys()):
+        var_list = ARGS[mode][var1]
+        # print(var_list)
+        if len(command_args) == 0: # list empty
+            command_args = [{var1: str(i)} for i in var_list]
+        else:
+            c = []
+            for y in var_list:
+                for x in command_args:
+                    z = deepcopy(x)
+                    z[var1] = str(y)
+                    c.append(z)
+            # print(c)
+            command_args = c
+    return command_args
 
-def run_dhtest():
-    for key_size in ARGS_DH['key_size']:
-        result = run_individual_test(['dh', str(key_size)])
-        pass
-
-def run_rsatest():
-    pass
-
-def main(mode):
+def main(arg):
     # make file
     print("-------------------------------")
     print("Building tests")
     print("-------------------------------\n")
+
     subprocess.run(['make'])
     if not os.path.exists(RESULT_DIR):
         os.makedirs(RESULT_DIR)
+
     print("\n-------------------------------")
     print("Build done")
     print("-------------------------------\n\n")
-    if mode == "bn":
-        run_bntest()
-    elif mode == "dh":
-        run_dhtest()
-    elif mode == "rsa":
-        run_rsatest()
-    elif mode == "all":
-        run_bntest()
-        run_dhtest()
-        run_rsatest()
-    else:
+
+    # check arg
+    try:
+        test_modes = []
+        if (arg == "all"):
+            test_modes = list(ARGS.keys())
+        else:
+            test_modes = [arg]
+        # print(test_modes)
+
+
+        for mode in test_modes:
+            command_args = get_params(mode)
+            # pprint(command_args)
+            csv_headers = sorted(ARGS[mode].keys())
+
+            test_data = []
+            for arg in command_args:
+                header = sorted(arg.keys())
+                a = [arg[k] for k in header]
+                print(arg)
+                command = [mode]
+                command.extend(a)
+                result = run_individual_test(command)
+
+                print(result, "\n")
+                row = arg.copy()
+                row_avg = arg.copy()
+                header.extend(['0.time', '0.time_avg'])
+                row['0.time'] = str(result)
+                row['0.time_avg'] = round(mean(result), 2)
+                test_data.append(row)
+
+            pprint(test_data)
+            filename = RESULT_DIR + mode + '.csv'
+            print_file_csv(filename, sorted(header), test_data)
+
+    except KeyError as e:
         print("invalid_args")
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    if (len(sys.argv) == 2):
+        main(sys.argv[1])
+    else:
+        print("invalid_args")
